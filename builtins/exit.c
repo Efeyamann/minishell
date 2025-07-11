6b1b573a9f3803346f29baa785a086d38a6ceb9d
Efeyamann <eyamansir@gmail.com>
2025-07-11 22:38:30 +0300

    $ implementation

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,35 +1,34 @@
 #include "../minishell.h"
 
-static void	print_display(void)
-{
-	write(2, "exit\n", 5);
-	write(2, "minishell: exit: too many arguments\n", 36);
-}
-
 void	exit_program(t_cmd *commands)
 {
 	int	status;
-	int	argc;
+	int	argc = 0;
 
-	argc = 0;
 	while (commands->args[argc])
 		argc++;
-	printf("%d\n", argc);
 	if (argc > 2)
-		print_display();
+	{
+		write(2, "exit\n", 5);
+		write(2, "minishell: exit: too many arguments\n", 36);
+		g_last_exit = 1;
+		return ;
+	}
+	write(2, "exit\n", 5);
+	if (argc == 1)
+		status = 0;
+	else if (is_numeric(commands->args[1]) == 0)
+	{
+		write(2, "minishell: exit: numeric argument required\n", 43);
+		g_last_exit = 255;
+		exit(255);
+	}
 	else
 	{
-		if (is_numeric(commands->args[1]) == 0)
-		{
-			write(2, "exit\n", 5);
-			write(2, "minishell: exit: numeric argument required\n", 43);
-		}
-		else
-		{
-			status = ft_atoi(commands->args[1]);
-			if (status < 0 || status > 255)
-				status = status % 256;
-			exit (status);
-		}
+		status = ft_atoi(commands->args[1]);
+		if (status < 0 || status > 255)
+			status %= 256;
+		g_last_exit = status;
 	}
+	exit(g_last_exit);
 }
